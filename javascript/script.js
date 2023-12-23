4b8e03fbcb84342f5b3905d630b82e4c53f8e80b
Edith Rand <edithrand505@gmail.com>
2023-12-23 18:33:25 +0100

    switch & case

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,72 @@
 const playBoard = document.querySelector(".play-board");
 
-//coordonnées initiales de la nourriture du serpent
+// Coordonnées initiales de la nourriture du serpent
 let foodX, foodY;
-let snakeX = 5 , snakeY = 10;
-let snakBody =[];
+let snakeX = 5, snakeY = 10;
+let snakBody = [];
 let velocityX = 0, velocityY = 0;
 
 /**
-la fonction JavaScript "Math.random()" qui renvoie un nombre aléatoire compris entre 0 et 1. Les valeurs retournées sont ensuite multipliées par 30 et arrondies vers le bas en utilisant la fonction JavaScript "Math.floor()". Les valeurs sont ensuite ajustées de 1 pour obtenir des coordonnées comprises entre 1 et 30 (inclus)
-**/
-const changeFoodPosition = ()=>{
-    foodX = Math.floor(Math.random()*30) + 1;
-    foodY = Math.floor(Math.random()*30) + 1;
-}
+ * La fonction "changeFoodPosition" utilise Math.random() pour générer des coordonnées
+ * aléatoires pour la nourriture du serpent.
+ */
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+};
 
-const changeDirection = (e)=>{
-   // console.log(e);
-    if(e.key === "ArrowUp"){
-        velocityX = 0;
-        velocityY = -1;
-    }else if(e.key === "ArrowDown"){
-        velocityX = 0;
-        velocityY = 1;
-    }else if(e.key === "ArrowLeft"){
-        velocityX = -1;
-        velocityY = 0;
-    }else if(e.key === "ArrowRight"){
-        velocityX = 1;
-        velocityY = 0;
+/**
+ * La fonction "changeDirection" ajuste les variables de vélocité en fonction de la touche
+ * fléchée appuyée par l'utilisateur.
+ */
+const changeDirection = (e) => {
+    switch (e.key) {
+        case "ArrowUp":
+            velocityX = 0;
+            velocityY = -1;
+            break;
+        case "ArrowDown":
+            velocityX = 0;
+            velocityY = 1;
+            break;
+        case "ArrowLeft":
+            velocityX = -1;
+            velocityY = 0;
+            break;
+        case "ArrowRight":
+            velocityX = 1;
+            velocityY = 0;
+            break;
     }
-}
+};
 
-// La fonction "initGame" crée une balise div avec la classe CSS "food" qui représente la nourriture du serpent et la place à l'aide de "grid-area" à des coordonnées spécifiées par les variables "foodX" et "foodY"
-const initGame =()=>{
+/**
+ * La fonction "initGame" initialise le jeu en créant la balise div représentant la
+ * nourriture du serpent et en mettant à jour la position du serpent.
+ */
+const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-    if(snakeX === foodX && snakeY === foodY){
+
+    if (snakeX === foodX && snakeY === foodY) {
         changeFoodPosition();
-        //ajouter l' element food(la position) sur un tableau du body
         snakBody.push([foodX, foodY]);
         console.log(snakBody);
     }
-    for(let i = snakBody.length - 1;i > 0; i--){
+
+    for (let i = snakBody.length - 1; i > 0; i--) {
         snakBody[i] = snakBody[i - 1];
     }
-    //initialisation du position du snack
-    snakBody[0] = [snakeX,snakeY];
-    //met à jour les variables "velocityX" et "velocityY" en fonction de la touche de direction appuyée,
+
+    snakBody[0] = [snakeX, snakeY];
     snakeX += velocityX;
     snakeY += velocityY;
-    for(let i = 0; i< snakBody.length; i++){
-         htmlMarkup += `<div class="head" style="grid-area: ${snakBody[i][1]} / ${snakBody[i][0]}"></div>`;
+
+    for (let i = 0; i < snakBody.length; i++) {
+        htmlMarkup += `<div class="head" style="grid-area: ${snakBody[i][1]} / ${snakBody[i][0]}"></div>`;
     }
-   
+
     playBoard.innerHTML = htmlMarkup;
-}
+};
 
 changeFoodPosition();
 setInterval(initGame, 125);
